34284ad0469b787e9df7744d0b611607e82b097f
gayi <gayathrimakam40@gmail.com>
2024-08-04 11:01:27 +0000

    Edited in branch

--- f1.sh
+++ f1.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
+echo " enter 1st "
+read a
+echo " enter  2nd "
+read b
+echo " choice any one opertion "
+read op
+case $op in
+	+) echo "$((a+b))"
+		;;
+        -) echo "$((a-b))"
+		;;
+	x) echo " $((axb))"
+		;;
+        %) echo "$((a%b))"
+		;;
+	/) echo "$((a/b))"
+		;;
+	*) echo " enter correct "
+esac
 
